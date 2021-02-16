69d1109ae0265d9d005da773354538fa2ea4663e
AuditiveSteuerung <79136769+AuditiveSteuerung@users.noreply.github.com>
2021-02-16 17:29:04 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,9 @@ sudo apt install python-gpiozero libespeak1 -y
 pip3 install pyttsx3  	
 pip3 install vosk
 pip3 install pyaudio
-wget https://alphacephei.com/vosk/models/vosk-model-small-de-zamia-0.3.zip
-unzip vosk-model-small-de-zamia-0.3.zip
-sudo mv vosk-model-small-de-zamia-0.3 model
+wget https://alphacephei.com/vosk/models/vosk-model-small-de-0.15.zip
+unzip vosk-model-small-de-0.15.zip
+sudo mv vosk-model-small-de-0.15 model
 git clone https://github.com/respeaker/seeed-voicecard #soundkarte microfon
 cd seeed-voicecard
 sudo ./install.sh
